92d140f341e3a597b40aeb337520f60047da908a
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-09-08 12:30:46 +0530

    Update App.jsx context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from "react";
-import { TodoForm } from './Components'
-import { TodoContext ,TodoProvider, UseTodo } from "./Contexts"
+import { useEffect, useState } from "react";
+import { TodoForm } from "./Components";
+import { TodoContext, TodoProvider, UseTodo } from "./Contexts";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo)=>{
+    setTodos((prev) => [...prev,{id: Date.now(),...todo}])
+  }
+  const updatetodo = (id,todo)=>{
+    setTodos((prev) => {prev.map((prevtodo)=>(prevtodo.id === id? todo:prevtodo))})
+  }
+  const deletetodo = (id)=>{
+    setTodos((prev) => prev.filter((prevtodo)=> prevtodo.id !==id))
+  }
+  const togglecomplete = (id)=>{
+    setTodos((prev) => prev.map((prevtodo)=>(prevtodo.id===id? {...prevtodo,completed:!prevtodo.completed}: prevtodo)))
+  }
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0) setTodos(todos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos" , JSON.stringify(todos))
+  },[todos])
+
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-        <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-          Manage Your Todos
-        </h1>
-        <div className="mb-4">{<TodoForm/>}</div>
-        <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+    <TodoProvider value={{todos,addTodo,updatetodo,deletetodo,togglecomplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{<TodoForm />}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
         </div>
       </div>
-    </div>
+    </TodoProvider>
   );
 }
 
